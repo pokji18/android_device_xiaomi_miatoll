90db25545ad2aa7b88eafd5f6c38f840ff40e140
Aciss <wawa22arpk@gmail.com>
2026-08-17 01:37:08 +0700

    Miatoll: Drop specific Clang configuration
    Removed additional flags for kernel build and GCC dependency.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,11 +72,8 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_RAMDISK_USE_LZ4 := true
 
-TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump
 TARGET_KERNEL_CONFIG := vendor/xiaomi/miatoll_defconfig
-TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6250
-TARGET_KERNEL_CLANG_VERSION := r563880c
 
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
